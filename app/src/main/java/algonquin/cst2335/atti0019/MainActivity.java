ea96a5c9b8e4ebdd685ce469fb55c75b97f372c9
Mohamed Attia <atti0019@algonquinlive.com>
2023-09-20 18:45:36 -0400

    Added Text View, Button, and Edit Text

--- app/src/main/java/algonquin/cst2335/atti0019/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/atti0019/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.atti0019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.myText);
+        Button b = findViewById(R.id.myButton);
+        EditText ed = findViewById(R.id.myEdit);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("U clicked the button");
+                b.setText("Why?");
+                ed.setText("Tell me why, NOW");
+
+            }
+        });
     }
 }
