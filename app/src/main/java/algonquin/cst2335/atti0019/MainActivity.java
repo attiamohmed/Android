63a3059e2bd7120faeeb436a8b7ce04252593205
Mohamed Attia <atti0019@algonquinlive.com>
2023-09-20 20:16:54 -0400

    Added Binding

--- app/src/main/java/algonquin/cst2335/atti0019/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/atti0019/MainActivity.java
@@ -2,25 +2,35 @@ package algonquin.cst2335.atti0019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import algonquin.cst2335.atti0019.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView tv = findViewById(R.id.myText);
-        Button b = findViewById(R.id.myButton);
-        EditText ed = findViewById(R.id.myEdit);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        TextView tv = binding.myText;
+        Button b = binding.myButton;
+        EditText ed = binding.myEdit;
 
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                tv.setText(R.string.hello_message);
+                b.setText(R.string.buttom_message);
+                ed.setText(R.string.edittext_message);
                 tv.setText("U clicked the button");
                 b.setText("Why?");
                 ed.setText("Tell me why, NOW");
