346571ee01842599acb04221073ef6a92ad6dbbf
Mohamed Attia <atti0019@algonquinlive.com>
2023-09-21 17:54:22 -0400

    Finished txet view

---
+++ app/src/main/java/algonquin/cst2335/atti0019/data/MainViewModel.java
@@ -0,0 +1,9 @@
+package algonquin.cst2335.atti0019.data;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+    public MutableLiveData<String> userString = new MutableLiveData("");
+}
--- app/src/main/java/algonquin/cst2335/atti0019/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/atti0019/ui/MainActivity.java
@@ -1,6 +1,8 @@
-package algonquin.cst2335.atti0019;
+package algonquin.cst2335.atti0019.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -9,31 +11,43 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import algonquin.cst2335.atti0019.R;
+import algonquin.cst2335.atti0019.data.MainViewModel;
 import algonquin.cst2335.atti0019.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         setContentView(binding.getRoot());
         TextView tv = binding.myText;
         Button b = binding.myButton;
         EditText ed = binding.myEdit;
+        ed.setText(viewModel.userString.getValue());
+        viewModel.userString.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                tv.setText("You wrote: " +s);
+            }
+        });
 
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tv.setText(R.string.hello_message);
-                b.setText(R.string.buttom_message);
-                ed.setText(R.string.edittext_message);
+                //tv.setText(R.string.hello_message);
+                //b.setText(R.string.buttom_message);
+                //ed.setText(R.string.edittext_message);
                 tv.setText("U clicked the button");
-                b.setText("Why?");
-                ed.setText("Tell me why, NOW");
+                // b.setText("Why?");
+                String string = ed.getText().toString();
+                viewModel.userString.postValue(string);
 
             }
         });
