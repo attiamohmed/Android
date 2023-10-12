267f77d91b95c7538b7ab3f2222cbd46d1fb0fb4
Mohamed Attia <atti0019@algonquinlive.com>
2023-10-11 22:04:09 -0400

    update

--- app/src/main/java/algonquin/cst2335/atti0019/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/atti0019/MainActivity.java
@@ -3,6 +3,7 @@ package algonquin.cst2335.atti0019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w( "MainActivity", "In onStart() - the app is now visible on the screen" );
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w( "MainActivity", "In onResume() - the app is now responding to user input " );
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w( "MainActivity", "In onPause() - the app is now is still visible to the user but its moved to the background " );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i( "MainActivity", "In onStop() - the app now is no longer visible to the user but still in the memory" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i( "MainActivity", "In onDestroy() - the app now is no longer in the memory" );
     }
 }
